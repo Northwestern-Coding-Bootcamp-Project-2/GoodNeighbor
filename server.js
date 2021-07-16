9577fbfc0e901f7c94bd72918ecba08558393bb8
Kyaah <kyaahh@gmail.com>
2021-07-15 19:19:07 -0500

    initializing homepage

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+// const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
+// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
+/*const sess = {
   secret: 'Super secret secret',
   cookie: {},
   resave: false,
@@ -19,12 +19,11 @@ const sess = {
   store: new SequelizeStore({
     db: sequelize,
   }),
-};
+};*/
 
-app.use(session(sess));
+// app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
-
+// 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
