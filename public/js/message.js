691057daff18fd6cc63941dbbbfcd93c541ff17a
rdm <80121987+rudie-g@users.noreply.github.com>
2021-07-18 17:44:00 -0500

    Update message.js

--- public/js/message.js
+++ public/js/message.js
@@ -1,16 +1,4 @@
-const messageFormHandler = async (event) => {
 
-const message = document.querySelector('#messages').value.trim();
-
-
-
-
-if (response.ok) {
-    document.location.replace('/');
-  } else {
-    alert(response.statusText);
-  }
-};
 
 
 
