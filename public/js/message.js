260da29e84e649cabbf6459648478016931f9c87
Kyaah <kyaahh@gmail.com>
2021-07-18 17:33:51 -0500

    message-handlebars

---
+++ public/js/message.js
@@ -0,0 +1,17 @@
+const messageFormHandler = async (event) => {
+
+const message = document.querySelector('#messages').value.trim();
+
+
+
+
+if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+
+
+
