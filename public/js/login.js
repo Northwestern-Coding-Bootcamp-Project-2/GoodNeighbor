9238f30a4ba98da9790ec413323cd3b902004ff7
Rudie G <rauoldm@gmail.com>
2021-07-18 14:39:58 -0500

    fixed login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,23 @@
 const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect values from the login form
-    const username = document.querySelector('#username').value.trim();
-    const password = document.querySelector('#password').value.trim();
-  
-    if (email && password) {
-      // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        // If successful, redirect the browser to the homepage page
-        document.location.replace('/homepage');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+  // Collect values from the login form
+  const username = document.querySelector('#username').value.trim();
+  const password = document.querySelector('#password').value.trim();
+  if (username && password) {
+    // Send a POST request to the API endpoint
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+      // If successful, redirect the browser to the homepage page
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
     }
-  };
-
-
+  }
+};
 document
 .querySelector('#login-submit')
 .addEventListener('click', loginFormHandler);
